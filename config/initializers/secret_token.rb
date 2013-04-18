4e22bc36ea951eee39f4d86faff2502926e9fb4b
yicui <yicui2004@gmail.com>
2013-04-18 12:29:28 -0500

    Project creation
    "rails new sample_app_rails"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppRails::Application.config.secret_token = 'f4bb38a68f5f217ae4c89f9be2fd579e44d4853fe076002ca447ea40499c7a0eac21a21763151f04f501786995fc8ae65340bd3cee9e16af926eca2b95a2c7f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppRails::Application.config.session_store :cookie_store, key: '_sample_app_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SampleAppRails::Application.config.session_store :active_record_store
