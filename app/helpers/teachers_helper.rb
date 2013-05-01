6f1d4198cebf0c8ff5f34770694f150106f3c19c
yicui <yicui2004@gmail.com>
2013-05-01 16:04:59 -0500

    Gravatar and Google map applied to teachers#show
    via Bootstrap thumbnail and jQuery-based GMap3 plugin

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,2 +1,7 @@
 module TeachersHelper
+ def gravatar_for(teacher) 
+    gravatar_id = Digest::MD5::hexdigest(teacher.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    link_to(image_tag(gravatar_url+"?s=200", alt: [teacher.first_name, teacher.last_name], class: "gravatar"), gravatar_url+"?s=400", class: "thumbnail span3")
+ end 
 end
